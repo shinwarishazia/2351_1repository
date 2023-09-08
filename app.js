fc30c5aca1ef019272cbe8c1e95d52c33d08b7ff
shinwarishazia <shinwarishazia01@gmail.com>
2023-09-08 12:08:29 -0500

    added more code

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-console.log(let);
 
 
 var let = 100;  // this is my bar  Number
-
+console.log(let);
 var name = "Dev Ed";  // This is the name display  string
 
 
 var checkout = false;  // Boolean
 
-var box = null;
-
+var box = "shazia";
 
 console.log(box);
+
+
+global_variable = 10;
+print //The value of the global variable is global_variable
+
+block_variable = 20;
+print  //The value of the block variable is block_variable
+
+sum = global_variable + block_variable  //The sum of the global and block variables is sum
+difference = global_variable - block_variable  //The difference of the global and block variables is difference
+product = global_variable * block_variable  //The product of the global and block variables is product
+quotient = global_variable / block_variable //The quotient of the global and block variables is quotient
+
+console.log("The value of the global variable is:");
+  /*The value of the block variable is: 20,
+  The sum of the global and block variables is: 30,
+  The difference of the global and block variables is: -10,
+  The product of the global and block variables is: 
+  The quotient of the global and block variables is:*/
+  
+
+string_variable = "Hello, world!"
+print //The value of the string variable is string_variable)
+//The value of the string variable is Hello, world!
+
+const object_variable = {
+  "name": "John Tim",
+  age: 30,
+  city: "New York"
+};
+console.log("The value of the object variable is:", object_variable);
+//The value of the object variable is: { name: 'John Tim', age: 30, city: 'New York' }
+
+object_variable['name'] = 'John Tim'
+object_variable['age'] = 30
+object_variable['city'] = 'New York'
+print //The value of the object variable is object_variable
